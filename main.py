6ad9089b586b1839fc9cf90a69e05a3cf3f92bf3
Eu20 <bogdanflorinel02@yahoo.ro>
2022-12-15 16:37:41 +0200

    added transcribe function

--- main.py
+++ main.py
@@ -21,3 +21,26 @@ def record(duration):
         sd.wait()
         print("Recording complete")
         write('output.mp3', fs, myrecording)
+
+def transcribe():
+        torch.cuda.is_available()
+        DEVICE = "cuda" if torch.cuda.is_available() else "cpu"
+
+        model = whisper.load_model("small", device = DEVICE)
+
+        audio = whisper.load_audio("output.mp3")
+
+        audio = whisper.pad_or_trim(audio) 
+
+        mel = whisper.log_mel_spectrogram(audio).to(model.device)
+
+        options = whisper.DecodingOptions(language="en", without_timestamps=True, fp16 =False)
+
+        result = whisper.decode(model, mel, options)
+
+        print(result.text)
+
+        result = model.transcribe('output.mp3')
+
+        print(result["text"])
+        return result["text"]
