0c135f38c393a1e704135f5d3bb62383b60e49ae
MariusHidra <mmalais@yahoo.com>
2022-12-15 18:25:48 +0200

    cm

--- main.py
+++ main.py
@@ -44,3 +44,27 @@ def transcribe():
 
         print(result["text"])
         return result["text"]
+
+def generate_email(text):
+        openai.api_key=os.getenv("OPENAI_API_KEY")
+        response=openai.Completion.create(
+                model="text-davinci-003",
+                prompt=f"Create a kind and formal email for this reason: {text}",
+                temperature=0.7,
+                max_tokens=256,
+                top_p=1,
+                frequency_penalty=0,
+                presence_penalty=0
+        )
+        return response["choices"][0]["text"]
+
+def main():
+        record(8)
+        text=transcribe()
+        email=generate_email(text)
+        print(email)
+        pyperclip.copy(email)
+
+if_name=="main_":
+main()
+
